538402ef934ba4d894ed0f87524e521fd59680ab
Kirimson <kierangates@outlook.com>
2022-05-22 23:58:53 +0100

    feat: some stuff for folders
    honestly doubt it works, but in theory can add Folder objects to the
    folder store using the add action, supplying a name for a folder to make
    Will be basis of the `mkdir` command

--- src/stores/folder.ts
+++ src/stores/folder.ts
@@ -1,11 +1,19 @@
 import { defineStore } from "pinia";
 
+import { Folder } from "@/models/Folder"
+
 export const useFolderStore = defineStore({
   id: "folder",
   state: () => ({
-    rawFolders: [],
+    rawFolders: [] as Folder[],
   }),
   getters: {
     folders: (state) => state.rawFolders,
-  }
+  },
+  actions: {
+    add(name:string) {
+      let newFolder = new Folder(name);
+      this.rawFolders.push(newFolder);
+    },
+  },
 });
