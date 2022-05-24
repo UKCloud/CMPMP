e87a834a19fca8a832d61731fa372d85e9e463f5
Kirimson <kierangates@outlook.com>
2022-05-24 23:37:06 +0100

    fix: change how config is stored

--- src/stores/config.ts
+++ src/stores/config.ts
@@ -1,12 +1,10 @@
 import { defineStore, type StoreDefinition } from "pinia";
-import defaultBg from "@/assets/img/bg.jpg";
+import defaultBg from "@/assets/img/default.png";
 import { useStorage} from '@vueuse/core'
 
 export const useConfigStore:StoreDefinition = defineStore({
   id: "config",
   state: () => useStorage('config', {
-    config: {
       background: defaultBg,
-    }
   }),
 });
