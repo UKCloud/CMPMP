8d47ab36faed0ad3935d9aaf1324d4026840e183
Kirimson <kierangates@outlook.com>
2022-05-25 00:59:12 +0100

    style:slightly better folder window

--- src/components/VaunchGui.vue
+++ src/components/VaunchGui.vue
@@ -16,15 +16,40 @@ export default defineComponent({
 <style scoped>
 #vaunch-folder-container {
   position: relative;
+  display: flex;
   width: 70vw;
   height: 65vh;
+  flex-direction: row;
+  justify-content: center;
+  flex-wrap: wrap;
+  padding: 1em;
+  font-size: 2rem;
+  align-items: center;
+  overflow-y: auto;
+}
+
+.vaunch-folder {
+  display: flex;
+  flex-direction: column;
+  margin: 0.5rem;
+  width: 30%;
+  height: 45%;
+}
+
+.folder-title {
+  position: relative;
+  width: 100%;
+  text-align: center;
+  background-color: rgba(0, 0, 0, 0.1);
+  /* border-radius: 5px; */
+  border-radius: 5px 5px 0 0 ;
 }
 </style>
 
 <template>
 <div id="vaunch-folder-container" class="vaunch-window">
-  <div v-for="folder in folders.items" :key="folder.name">
-    {{ folder.name }}
+  <div v-for="folder in folders.items" :key="folder.name" class="vaunch-folder vaunch-window">
+    <span class="folder-title">{{ folder.name }}</span>
   </div>
 </div>
 </template>
