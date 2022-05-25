3f3e120edcdf4ce1a5ae2353685e50530092763b
Kirimson <kierangates@outlook.com>
2022-05-25 01:24:36 +0100

    fix:tab complete will not delete last word anymore

--- src/components/VaunchInput.vue
+++ src/components/VaunchInput.vue
@@ -28,12 +28,12 @@ export default defineComponent({
 
       // Get the current word that is being typed 
       let lastWord: string = input[input.length - 1];
+
       // Remove the last word, so it wont be considered in the autocomplete placeholder text
       input.pop();
 
       // If on the second+ word, prefix the autocomplete text with the previous words
       if (input.length > 0) {
-        this.autocomplete = input.join(" ");
         this.autocomplete += " "; // Add extra space for the to-be-completed word
       }
 
@@ -41,12 +41,15 @@ export default defineComponent({
       // Only do this on the first "word", as commands will always be the first word
       if (lastWord.length > 0 && input.length == 0) {
         this.autocomplete += this.getAutocomplete(lastWord, commands);
-      }
+      } else this.autocomplete = val;
     },
   },
   methods: {
     complete() {
-      this.vaunchInput = this.autocomplete + " ";
+      // Only complete if there is something to complete
+      if (this.autocomplete.length > this.vaunchInput.length) {
+        this.vaunchInput = this.autocomplete + " ";
+      }
     },
     sendCommand() {
       this.$emit("command", this.vaunchInput.split(' '))
