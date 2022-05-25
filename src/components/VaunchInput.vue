043d1ace5079365a309ddafcb3b292e3e92c8c47
Kirimson <kierangates@outlook.com>
2022-05-25 21:54:48 +0100

    fix:file autocomplete

--- src/components/VaunchInput.vue
+++ src/components/VaunchInput.vue
@@ -57,7 +57,11 @@ export default defineComponent({
       if (lastWord.length > 0 && this.completeType == "") {
         // If on the second+ word, check folder names/files to autocomplete
         this.autocomplete += this.getAutocompleteFolder(lastWord, this.folders.folderNames);
-        
+        let pathSplit = lastWord.split("/")
+        if (this.completeType == "" && lastWord.includes("/") && pathSplit[1].length > 0) {
+          let folder:VaunchFolder = this.folders.getFolderByName(pathSplit[0]);
+          this.autocomplete += pathSplit[0] + "/" + this.getAutocompleteFile(pathSplit[1], folder.getFiles());
+        }
       }
       // If no autocomplete was successful, set it to the current value
       if (this.completeType == "") {
