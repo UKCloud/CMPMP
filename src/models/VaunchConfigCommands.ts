e64318737ad03820ade304fc50b14fa9fa7ac042
Kieran Gates <kierangates@outlook.com>
2022-06-03 00:32:11 +0100

    docs: import documented

--- src/models/VaunchConfigCommands.ts
+++ src/models/VaunchConfigCommands.ts
@@ -329,7 +329,55 @@ export class VaunchExport extends VaunchCommand {
 
 export class VaunchImport extends VaunchCommand {
   constructor() {
-    super("import");
+    let longDescription:string[] = ["Imports an exported Vaunch configuration into this instance. Files/Folders will be imported by default, only adding new folders.",
+      "Config can additionally be imported, as well as force importing all folders, deleting all of your current folders.",
+      "Folder content can additionally be merged with the export file if desired"]
+    let parameters:Parameter[] = [{
+      name: "-f",
+      optional: true,
+      repeatable: false,
+    },
+    {
+      name: "-m",
+      optional: true,
+      repeatable: false,
+    },
+    {
+      name: "files",
+      optional: true,
+      repeatable: false,
+    },
+    {
+      name: "config",
+      optional: true,
+      repeatable: false,
+    }]
+    let examples:Example[] = [{
+      args: [],
+      description: ["Imports only new folders from the exported file.",
+        "If the export has a folder called 'sites' and so does your current instance, this folder will be skipped"]
+    },
+    {
+      args: ["files"],
+      description: ["Functionally equivalent to running 'import' with no arguments"]
+    },
+    {
+      args: ["config"],
+      description: ["Imports only the config from the exported file, such as the colour scheme, GUI visibility, and default search file."]
+    },
+    {
+      args: ["files", "config"],
+      description: ["Imports both files/folders and config from the exported file.", "Existing folders will be skipped"]
+    },
+    {
+      args: ["-m"],
+      description: ["Imports files/folders and merges existing folders from the export, adding new files into existing folders"]
+    },
+    {
+      args: ["-f"],
+      description: ["Force imports files/folders into Vaunch, deleting all current folders before replacing them with the content of the exported file"]
+    }]
+    super("import", longDescription, parameters, examples);
   }
   description = "Imports vaunch from a file";
 
