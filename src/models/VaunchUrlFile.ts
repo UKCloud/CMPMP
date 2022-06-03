b9d565f24c391b589744376289a15d143df2f739
Kieran Gates <kierangates@outlook.com>
2022-06-03 12:57:38 +0100

    feat:can go directly to URLs fix:fuzzy fiels only executes with fuzzy on
    Enerting a URL into the command input will navigate there.
    Makes a temp VaunchLink to test the URL
    VaunchURLFile createUrl has extra conditions to returns a URL
    Modified VaunchQuery getDescription to always return this.content if
    no description is set, as new URL checking failed with ${}

--- src/models/VaunchUrlFile.ts
+++ src/models/VaunchUrlFile.ts
@@ -3,14 +3,14 @@ import type { VaunchFolder } from "./VaunchFolder";
 
 export abstract class VaunchUrlFile extends VaunchFile {
 
-  parent:VaunchFolder;
+  parent:VaunchFolder|undefined;
   filetype:string = "VaunchUrlFile";
 
-  constructor(name:string, parent:VaunchFolder, icon:string, iconClass:string, hits:number = 0, description:string="") {
+  constructor(name:string, parent:VaunchFolder|undefined, icon:string, iconClass:string, hits:number = 0, description:string="") {
     super(name, icon, iconClass, hits);
-    this.parent = parent;
     this.description = description
-    if (this.parent) {
+    if (parent) {
+      this.parent = parent;
       this.aliases.push(`${this.parent.name}/${this.fileName}`);
     }
   }
@@ -23,18 +23,30 @@ export abstract class VaunchUrlFile extends VaunchFile {
 
   protected createUrl(url:string = this.content): URL|undefined {
     try {
-      let urlString:string = this.prependHttps(url);
-      return new URL(urlString);
+      // If passed url starts with http/https already, attempt to return it as a URL
+      // otherwise prepend https:// to it.
+      let httpPrefixedTest:RegExp = /^https?:\/\//g
+      if (httpPrefixedTest.test(url)) {
+        return new URL(url);
+      } else url = this.prependHttps(url);
+      
+      // Now with https:// prepended, run an additional URL test against the string
+      let fullUrlTest:RegExp = /^(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)$/g
+      if (fullUrlTest.test(url)) {
+        return new URL(url);
+      } else return undefined;
     } catch (e) {
       return undefined
     }
   }
 
   protected getParentName(titleCase:boolean=false):string {
-    if (titleCase) {
-      return this.parent.titleCase();
-    }
-    return this.parent.name;
+    if (this.parent) {
+      if (titleCase) {
+        return this.parent.titleCase();
+      }
+      return this.parent.name;
+    } else return "";
   }
 
   getCorrectURL(): string {
