e2f4564e6066d87568732b503792c326f635c7ae
Kieran Gates <kierangates@outlook.com>
2022-06-14 23:14:38 +0100

    refactor:Tooltip Composition API

--- src/components/VaunchTooltip.vue
+++ src/components/VaunchTooltip.vue
@@ -1,55 +1,49 @@
-<script lang="ts">
-import { defineComponent } from "vue";
+<script setup lang="ts">
+import { onMounted } from "vue";
+import { useConfigStore } from "@/stores/config";
 
 import tippy, { followCursor } from "tippy.js/headless";
 import "tippy.js/dist/tippy.css";
-import { useConfigStore } from "@/stores/config";
 
-export default defineComponent({
-  name: "VaunchTooltip",
-  props: ["tipFor", "tipFile"],
-  mounted() {
-    tippy("#" + this.tipFor, {
-      content: this.tipFile,
-      followCursor: true,
-      plugins: [followCursor],
-      arrow: false,
-      placement: "right-end",
-      render(instance: any) {
-        // The recommended structure is to use the popper as an outer wrapper
-        // element, with an inner `box` element
-        const popper = document.createElement("div");
-        const box = document.createElement("div");
+const props = defineProps(["tipFor","tipFile"]);
 
-        popper.appendChild(box);
+onMounted(() => {
+  tippy("#" + props.tipFor, {
+  content: props.tipFile,
+  followCursor: true,
+  plugins: [followCursor],
+  arrow: false,
+  placement: "right-end",
+  render(instance: any) {
+    // The recommended structure is to use the popper as an outer wrapper
+    // element, with an inner `box` element
+    const popper = document.createElement("div");
+    const box = document.createElement("div");
 
-        box.classList.add("tippy-box", "vaunch-tippy", "vaunch-window");
-        box.textContent = instance.props.content.getDescription();
+    popper.appendChild(box);
 
-        const config = useConfigStore();
-        function onUpdate(prevProps: any, nextProps: any) {
-          // DOM diffing
-          if (prevProps.content !== nextProps.content.getDescription()) {
-            box.textContent = nextProps.content.getDescription();
-          }
-          // Color diffing
-          if (
-            box.style.background != config.color.windowOpaque ||
-            box.style.color != config.color.text
-          ) {
-            box.style.background = config.color.windowOpaque;
-            box.style.color = config.color.text;
-          }
-        }
+    box.classList.add("tippy-box", "vaunch-tippy", "vaunch-window");
+    box.textContent = instance.props.content.getDescription();
 
-        return {
-          popper,
-          onUpdate,
-        };
-      },
-    });
-  },
-});
+    const config = useConfigStore();
+    function onUpdate(prevProps: any, nextProps: any) {
+      // DOM diffing
+      if (prevProps.content !== nextProps.content.getDescription()) {
+        box.textContent = nextProps.content.getDescription();
+      }
+      // Color diffing
+      if (
+        box.style.background != config.color.windowOpaque ||
+        box.style.color != config.color.text
+      ) {
+        box.style.background = config.color.windowOpaque;
+        box.style.color = config.color.text;
+      }
+    }
+    return {popper,onUpdate};
+    },
+  });
+})
 </script>
 
 <style>
