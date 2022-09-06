aa9eca4681ca10bf2a05b3fb0de05ea36ee1cdf1
Sonam Pal <spal@ukcloud.com>
2022-09-06 15:02:03 +0100

    Fontawesome downloaded and converted to typescript

--- setupScript.ts
+++ setupScript.ts
@@ -1,38 +1,28 @@
-const https = require("https");
-const fs = require("fs");
-const path = require("path");
-const decompress = require("decompress");
-const AdmZip = require("adm-zip")
+import * as https from "https";
+import * as fs from "fs";
+import * as path from 'path';
+import * as decompress from "decompress";
 
-const url = "https://use.fontawesome.com/releases/v6.2.0/fontawesome-free-6.2.0-web.zip";
-
-const zipPath = path.join("src", "assets");
-
+const url: string = "https://use.fontawesome.com/releases/v6.2.0/fontawesome-free-6.2.0-web.zip";
+const zipPath: string = path.join("src", "assets");
 fs.mkdirSync(zipPath, { recursive: true });
-
-const fontAwesomeFile = "fontawesome.zip";
-const fontAwesomeFilePath = path.join(zipPath, fontAwesomeFile);
-const fontAwesomeUnzippedFile = "fontawesome";
-const fontAwesomeUnzippedFilePath = path.join(zipPath, fontAwesomeUnzippedFile);
+const fontAwesomeFile: string = "fontawesome.zip";
+const fontAwesomeFilePath: string = path.join(zipPath, fontAwesomeFile);
+const fontAwesomeUnzippedFile: string = "fontawesome";
+const destFilePath: string = path.join(zipPath, fontAwesomeUnzippedFile);
 const file = fs.createWriteStream(fontAwesomeFilePath);
-const unzippedFile = fs.createWriteStream(fontAwesomeUnzippedFilePath);
-const currZipPath = path.join(zipPath, "fontawesome-free-6.2.0-web")
+const currPath: string = path.join(zipPath, "fontawesome-free-6.2.0-web")
 
 https.get(url, function (response) {
-    response.pipe(file, unzippedFile);
+    response.pipe(file);
     file.on("finish", () => {
         file.close();
         console.log("Download Completed");
-        const unzipFile = decompress(fontAwesomeFilePath, zipPath);
-        console.log("Unzipped");
-
+        decompress(fontAwesomeFilePath, zipPath).then(() => {
+            fs.rename(currPath, destFilePath, () => {
+                console.log("Download unzipped")
+                fs.unlinkSync(fontAwesomeFilePath);
+            })
+        })
     });
 });
-
-try {
-    fs.renameSync(currZipPath, fontAwesomeUnzippedFilePath);
-    console.log("Renamed the directory.")
-} catch (err) {
-    console.log(err)
-}
-
