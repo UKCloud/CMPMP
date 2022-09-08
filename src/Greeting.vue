5acbbf7ec4193a9aac7c6119faa0ac3228e581c0
alexiswillowp <alexiswillowpaterson@gmail.com>
2022-09-08 08:52:39 +0100

    initial commit

---
+++ src/Greeting.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="greet">
+        <h3 v-if="email">Welcome {{email}}</h3>
+        <h3 v-else>you are not logged in</h3>
+    </div>
+</template>
+
+<script setup lang="ts">
+    defineProps<{
+        email: string;
+    }>();
+</script>
+
+<style></style>
+
